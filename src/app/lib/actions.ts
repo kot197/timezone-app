a50e5f0a2d336a7c2c8fcd5cd82e9b4449436f91
kot197 <160315848+kot197@users.noreply.github.com>
2024-09-19 21:10:39 +0700

    adding selection items
    installed country-and-timezones
    installed tailwind ui forms

---
+++ src/app/lib/actions.ts
@@ -0,0 +1,7 @@
+"use server"
+
+import { prisma } from "@/db/prisma"
+
+export async function createFriend(formData: FormData) {
+    
+}
--- src/app/ui/add-friend/add-friend-form.tsx
+++ src/app/ui/add-friend/add-friend-form.tsx
@@ -1,20 +1,33 @@
+import { getAllCountries } from "countries-and-timezones";
+
 
 export default function AddFriendForm() {
+    const countries = getAllCountries(); // Get all countries with timezone
+
+    const selectItems = Object.entries(countries).map(([code, country]) => {
+        return (
+            <option key={code} value={country.name}>
+                { country.name }
+            </option>
+        );
+    })
+
     return (
         <form className="p-20">
             <div className="flex flex-col py-4">
                 <label className="pb-4 text-xl" htmlFor="name">Name</label>
-                <input className="bg-transparent border rounded-lg py-2 px-2" placeholder="Enter Name" type="text" id="name" name="name" required></input>
+                <input className="bg-transparent border-white border rounded-lg py-2 px-2" placeholder="Enter Name" type="text" id="name" name="name" required></input>
             </div>
             <div className="flex flex-col py-4">
                 <label className="pb-4 text-xl" htmlFor="timezone">Timezone</label>
-                <select className="bg-transparent border rounded-lg py-2" id="timezone" name="timezone">
+                <select className="bg-transparent border-white border rounded-lg py-2 *:text-black" id="timezone" name="timezone">
                     <option value="">Select a Timezone</option>
+                    { selectItems }
                 </select>
             </div>
             <div className="flex flex-col py-4">
                 <label className="pb-4 text-xl" htmlFor="location">Location (Optional)</label>
-                <input className="bg-transparent border rounded-lg py-2 px-2" placeholder="Enter Location" type="text" id="location" name="location"></input>
+                <input className="bg-transparent border-white border rounded-lg py-2 px-2" placeholder="Enter Location" type="text" id="location" name="location"></input>
             </div>
         </form>
     );
